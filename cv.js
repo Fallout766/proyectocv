c2e3505b7bee90cf58b7e573309db5849e2eddbe
Fallout766 <123282540+Fallout766@users.noreply.github.com>
2023-01-31 01:48:32 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,6 @@
+
+/**Lo que me costó hacer esto por el amor de Dios!*/
+document.getElementById("boton").addEventListener ("click", function () {
+    console.log("Muestra foto de perfil");
+    document.getElementById("fotoperfil").hidden=false;
+})
